280ca146a03684e3459f8c44997df1b7f91e7325
Osée Mukaya <mukayatshimanga@gmail.com>
2021-05-06 09:51:42 +0100

    connected at mongodb database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const PORT = 3000;
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://osee:osee@cluster0.kahm9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connexion succeffully in database");
+    }
+  }
+);
 
 //Middlewares
 app.use(morgan("dev"));
